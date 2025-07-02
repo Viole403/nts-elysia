9d1318951422d384f4a84066d05ab20fbaa25141
Masalief Maulana <m.maulana.fb26@gmail.com>
2025-07-02 07:32:05 +0700

    feat(index): restructure app initialization and middleware setup
    - Refactor the app initialization to improve readability and organization.
    - Add middleware for CORS, Swagger, JWT, and Bearer authentication.
    - Integrate various modules including auth, user, articles, and subscriptions.
    - Ensure the app listens on the specified port with improved logging.
    
    This change enhances the modularity and maintainability of the codebase.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,70 @@
-import { Elysia } from "elysia";
+import { Elysia, Context } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
+import { cors } from '@elysiajs/cors';
+import { jwt } from '@elysiajs/jwt';
+import { bearer } from '@elysiajs/bearer';
+import { oauth2Plugin } from './plugins/oauth2';
+import { authModule } from './modules/auth';
+import { articlesModule } from './modules/articles';
+import { commentsModule } from './modules/comments';
+import { reactionsModule } from './modules/reactions';
+import { paymentsModule } from './modules/payments';
+import { cronModule } from './modules/cron';
+import { userModule } from './modules/users';
+import { notificationsModule } from './modules/notifications';
+import { groupModule } from './modules/groups';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { jobModule } from './modules/jobs';
+import { courseModule } from './modules/courses';
+import { eventModule } from './modules/events';
+import { shopModule } from './modules/shop';
+import { subscriptionModule } from './modules/subscriptions';
+import { cardModule } from './modules/cards';
+import { paymentAccountModule } from './modules/accounts';
+import { beneficiaryModule } from './modules/beneficiaries';
+import { payoutModule } from './modules/payouts';
+import { rbac } from './plugins/rbac';
+import { UserRole } from '@prisma/client';
+
+const app = new Elysia();
+
+app.use(cors());
+app.use(swagger());
+app.use(jwt({
+  name: 'jwt',
+  secret: process.env.JWT_SECRET || 'supersecret',
+}));
+app.use(bearer());
+app.use(oauth2Plugin);
+
+app.use(authModule);
+app.use(userModule);
+app.use(articlesModule);
+app.use(groupModule);
+
+app.use(jobModule);
+app.use(courseModule);
+app.use(eventModule);
+app.use(shopModule);
+app.use(commentsModule);
+app.use(reactionsModule);
+app.use(notificationsModule);
+app.use(paymentsModule);
+app.use(subscriptionModule);
+app.use(cardModule);
+app.use(paymentAccountModule);
+app.use(beneficiaryModule);
+app.use(payoutModule);
+app.use(cronModule);
+
+// Example protected admin route
+
+
+app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `
+    App is running at http://${app.server?.hostname}:${app.server?.port}
+  `
 );
+export default app;
